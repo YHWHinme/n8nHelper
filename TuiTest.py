5425ed7660b005b64199712b193924a894d3c833
YHWHinme <brokenmopflag@gmail.com>
2026-02-24 22:24:32 +0000

    Learnt a bunch

--- TuiTest.py
+++ TuiTest.py
@@ -1,8 +1,19 @@
 from textual.app import App
-from textual.widgets import Static
+from textual.widgets import Label, Static
+
 
 class HelloWorld(App):
+    CSS_PATH = "./styled.css"
+
     def compose(self):
-        yield Static("HelloWorld")
+        yield Static("HelloWorld!", classes="helloStyle1")
+        yield Label("What is your[red bold] name?[red/] Child?", classes="style1")
+
+    def on_key(self, event):
+        match event.on_key:
+            case "q":
+                exit()
+
+
 if __name__ == "__main__":
     HelloWorld().run()
